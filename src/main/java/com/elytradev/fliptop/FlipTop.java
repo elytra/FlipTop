bb99fb6a62a0461061bdb7a061323a4a8747c1be
Meredith Espinosa <goclonefilms@gmail.com>
2018-02-24 02:17:08 -0800

    seriously fuck loot tables.

--- src/main/java/com/elytradev/fliptop/FlipTop.java
+++ src/main/java/com/elytradev/fliptop/FlipTop.java
@@ -97,7 +97,7 @@ public class FlipTop {
         public void lootLoad(LootTableLoadEvent evt) {
             if (evt.getName().equals(LootTableList.ENTITIES_IRON_GOLEM)) {
                 // do stuff with evt.getTable()
-                LootEntry entry = new LootEntryTable(new ResourceLocation("fliptop:inject/iron_golem"), 1, 1, new LootCondition[0], "golem_entry"); // weight doesn't matter since it's the only entry in the pool. Other params set as you wish.
+                LootEntry entry = new LootEntryTable(new ResourceLocation("fliptop","inject/iron_golem"), 1, 0, new LootCondition[0], "golem_entry"); // weight doesn't matter since it's the only entry in the pool. Other params set as you wish.
 
                 LootPool pool = new LootPool(new LootEntry[] {entry}, new LootCondition[0], new RandomValueRange(1), new RandomValueRange(0, 1), "golem_pool"); // Other params set as you wish.
 
